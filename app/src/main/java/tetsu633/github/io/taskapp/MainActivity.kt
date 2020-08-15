399cfd6e854bd139c198f8186717f1a58882f309
tetsu633 <tetsunishimura633@gmail.com>
2020-08-15 20:50:56 +0900

    メイン画面リストビュー追加、新規・編集画面レイアウト追加

--- app/src/main/java/tetsu633/github/io/taskapp/MainActivity.kt
+++ app/src/main/java/tetsu633/github/io/taskapp/MainActivity.kt
@@ -1,12 +1,31 @@
 package tetsu633.github.io.taskapp
 
-import android.support.v7.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var array: ArrayList<String> = arrayListOf("test", "test1", "test2")
+
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, android.R.id.text1, array)
+        listView.adapter = adapter
+        listView.onItemClickListener = OnItemClickListener()
+    }
+
+    inner class OnItemClickListener: AdapterView.OnItemClickListener {
+        override fun onItemClick(parent: AdapterView<*>, view: View, position: Int, id: Long) {
+            val intent = Intent(this@MainActivity, NewForm::class.java)
+            intent.putExtra("text", (view.findViewById<TextView>(android.R.id.text1)).text)
+            startActivity(intent)
+        }
     }
 }
+
---
+++ app/src/main/java/tetsu633/github/io/taskapp/NewForm.kt
@@ -0,0 +1,14 @@
+package tetsu633.github.io.taskapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_new_form.*
+
+class NewForm : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_form)
+
+    }
+}
